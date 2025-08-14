1ae661f31e272901c8eb166cf158ada6e0294352
Korgo <hello@korgo.net>
2025-08-14 16:12:22 +0800

    QoL: Limit max container width

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-zinc-950 w-screen h-screen p-6 text-cx-text-subtle bg-zebra">
-    <div class="bg-gray-950 rounded-lg border border-cx-border w-full h-full">
+    <div class="bg-gray-950 rounded-lg border border-cx-border w-full h-full max-w-[1376px] mx-auto">
       <NuxtPage />
     </div>
   </div>
